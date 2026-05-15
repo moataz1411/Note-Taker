40e4eeef088f5e5209fa5be7428c57defd88c518
moataz1411 <mezoahmed1411@gmail.com>
2026-05-15 17:27:21 +0300

    implement note creation and deletion functionality in Note Taker app

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const notecontainer=document.querySelector(".notes-container");
 const createbtn=document.querySelector(".btn");
 let notes=document.querySelectorAll(".input-box");
 createbtn.addEventListener("click",()=>{
-
-})
+    let note = document.createElement("div");
+    let inputBox=document.createElement("p");
+    let img=document.createElement("img");
+    note.className="note"
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src="images/delete.png";
+    img.className="delete";
+    note.appendChild(inputBox);
+    note.appendChild(img);
+    notecontainer.appendChild(note);
+    img.addEventListener("click",()=>{
+        note.remove();
+    });
+});
 
 
 
