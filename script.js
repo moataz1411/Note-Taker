d77f885264977004e9ce7a80ef1ac889c4b2139d
moataz1411 <mezoahmed1411@gmail.com>
2026-05-15 17:05:10 +0300

    add initial JavaScript file for Note Taker app functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+const notecontainer=document.querySelector(".notes-container");
+const createbtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+createbtn.addEventListener("click",()=>{
+
+})
+
+
+
+
+
+
